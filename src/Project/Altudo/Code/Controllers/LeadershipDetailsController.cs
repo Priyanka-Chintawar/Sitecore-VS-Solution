a02283ec1a97e883aed39793866b5f97834b0a38
Priyanka-Chintawar <priyanka.chintawar@altudo.co>
2022-02-11 12:27:32 +0530

    First commit

---
+++ src/Project/Altudo/Code/Controllers/LeadershipDetailsController.cs
@@ -0,0 +1,42 @@
+﻿using AltudoBatch1.Project.Altudo.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Links;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AltudoBatch1.Project.Altudo.Controllers
+{
+    public class LeadershipDetailsController : Controller
+    {
+        // GET: LeadershipDetails
+        public ActionResult Index()
+        {
+            var contextItem = Sitecore.Context.Item;
+
+            LinkField linkfield = contextItem.Fields["SuggestedArticle"];
+
+            var targetItem = linkfield.TargetItem;
+
+            LeadershipDetails leadership = new LeadershipDetails
+            {
+                Name = new HtmlString(FieldRenderer.Render(contextItem,"Name")),
+                Designation = new HtmlString(contextItem.Fields["Designation"].Value),
+                ContactNumber = new HtmlString(FieldRenderer.Render(contextItem, "ContactNumber")),
+                ProfileBrief = new HtmlString(FieldRenderer.Render(contextItem, "ProfileBrief")),
+                ProfilePicture = new HtmlString(FieldRenderer.Render(contextItem, "ProfilePicture")),
+                SuggestedArticleUrl = linkfield.Url,
+                SuggestedArticleText = linkfield.Text
+
+                //LinkManager.GetItemUrl(targetItem),targetItem.Fields["Title"].Value
+
+
+            };
+
+            return View("/Views/Altudo/LeadershipDetails.cshtml", leadership);
+        }
+    }
+}
---
+++ src/Project/Altudo/Code/Controllers/ListOfLeadersController.cs
@@ -0,0 +1,38 @@
+﻿using AltudoBatch1.Project.Altudo.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AltudoBatch1.Project.Altudo.Controllers
+{
+    public class ListOfLeadersController : Controller
+    {
+        // GET: ListOfLeaders
+        public ActionResult Index()
+        {
+            var contextItem = Sitecore.Context.Item;
+
+            List<LeadershipDetails> listofLeader = new List<LeadershipDetails>();
+
+            MultilistField leaders = contextItem.Fields["Leaders"];
+            listofLeader = leaders.GetItems()
+            .Select(x => new LeadershipDetails
+            {
+                Name = new HtmlString(FieldRenderer.Render(x, "Name")),
+                Designation = new HtmlString(FieldRenderer.Render(x, "Designation")),
+                ContactNumber = new HtmlString(FieldRenderer.Render(x, "ContactNumber")),
+                ProfileBrief = new HtmlString(FieldRenderer.Render(x, "ProfileBrief")),
+                ProfilePicture = new HtmlString(FieldRenderer.Render(x, "ProfilePicture"))
+                //SuggestedArticleUrl = linkfield.Url,
+                //SuggestedArticleText = linkfield.Text
+            }).ToList();
+
+
+            return View("/Views/Altudo/ListOfLeaders.cshtml", listofLeader);
+        }
+    }
+}
